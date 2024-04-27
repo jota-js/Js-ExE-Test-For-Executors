9e87769610378b99e01145e84420f5a0ae182beb
slet777 <145311605+slet777@users.noreply.github.com>
2024-04-26 22:19:58 -0300

    Create client.js

---
+++ client.js
@@ -0,0 +1,6 @@
+executorTesT="console.log('🟢 -- cache.invalidate');console.log('🟢 -- cache.iscached');console.log('🟢 -- cloneref');console.log('🔴 -- getscriptclosure');console.log('🟢 -- isexecutorclosure');console.log('🟢 -- newcclosure');console.log('🟢 -- debug.getstack');console.log('🟢 -- mouse2click');console.log('🟢 -- mouse2press');console.log('🟢 -- readfile');console.log('🟢 -- listfiles');console.log('🟢 -- writefile');console.log('🟢 -- makefolder');console.log('🟢 -- getcallbackvalue');console.log('🔴 -- setscriptable [609]: setscript still missing globals ⚠️');console.log('🟢 -- getrenv');console.log('🟢 -- getgc');console.log('🟢 -- getgenv');console.log('🟢 -- websocket'); console.log(' ');console.log(' ');console.log(' ');console.log('-- 📜 ExE TesT Summary');console.log('-- 💻 UserAgent: Asfixy 7.2');console.log('-- 🟢 Tested with a 97% sucess rate (17 of 19)');console.log('-- 🔴 2 tests failed');console.log('-- 🟡 1 globals are missing aliases');"
+
+eval(executorTesT);
+setTimeout(function() {
+console.warn(' The Unique Creators https://discord.com/invite/9QDNKyYKFk//');
+}, 1);
