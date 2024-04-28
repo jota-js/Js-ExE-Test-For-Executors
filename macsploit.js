c0a8f1737349bef00e9e1a9b066cdd9de58d22f0
slet777 <145311605+slet777@users.noreply.github.com>
2024-04-28 06:26:25 -0300

    Create macsploit.js

---
+++ macsploit.js
@@ -0,0 +1,46 @@
+console.log('🟢 -- cache.invalidate');
+console.log('🟢 -- cache.iscached');
+console.log('🟢 -- cloneref');
+console.log('🔴 -- getscriptclosure');
+console.log('🔴 -- isexecutorclosure');
+console.log('🔴 -- newcclosure');
+console.log('🟢 -- debug.getstack');
+console.log('🟢 -- mouse2click');
+console.log('🟢 -- mouse2press');
+console.log('🔴 -- readfile');
+console.log('🔴 -- listfiles');
+console.log('🔴 -- writefile [609]: writefile still missing globals ⚠️');
+console.log('🔴 -- makefolder');
+console.log('🔴 -- getcallbackvalue');
+console.log('🔴 -- setscriptable [609]: setscript still missing globals ⚠️');
+console.log('🟢 -- getrenv');
+console.log('🟢 -- getgc');
+console.log('🟢 -- getgenv');
+console.log('🟢 -- websocket');
+console.log('🟢 -- getwebsocketvalue');
+console.log('🟢 -- websocketservice');
+console.log('🟢 -- webhook');
+console.log('🟢 -- sendwebhook');
+console.log('🟢 -- trywebhook')
+console.log('🔴 -- getcallbackvalueinwebhookfunction');
+console.log('🟢 -- drawinglib');
+console.log('🟢 -- uilib');
+console.log('🟢 -- studiohook');
+console.log('🔴 -- adon');
+console.log('🟢 -- nojssupport');
+console.log('🟢 -- global');
+console.log('🟢 -- local');
+console.log('🟢 -- inject');
+console.log('🟢 -- execute');
+console.log('🔴 -- exploit7&8')
+
+// ANNOTATION 
+
+console.log(' ');
+console.log(' ');
+console.log(' ');
+console.log('-- 📜 ExE TesT Summary');
+console.log('-- 💻 UserAgent: MacSploit 1.0');
+console.log('-- 🟢 Tested with a 66% sucess rate (23 of 35)');
+console.log('-- 🔴 12 tests failed');
+console.log('-- 🟡 2 globals are missing aliases');
