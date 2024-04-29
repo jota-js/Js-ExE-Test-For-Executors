ff7caacbb8743857c64984cc6bc83c4591b7368d
slet777 <145311605+slet777@users.noreply.github.com>
2024-04-29 19:29:07 -0300

    Update alternative-client.js

--- alternative-client.js
+++ alternative-client.js
@@ -1,6 +1,48 @@
-executorTesT="console.log('🟢 -- cache.invalidate');console.log('🟢 -- cache.iscached');console.log('🟢 -- cloneref');console.log('🟢 -- getscriptclosure');console.log('🟢 -- isexecutorclosure');console.log('🟢 -- newcclosure');console.log('🟢 -- debug.getstack');console.log('🟢 -- mouse2click');console.log('🟢 -- mouse2press');console.log('🟢 -- readfile');console.log('🟢 -- listfiles');console.log('🟢 -- writefile');console.log('🟢 -- makefolder');console.log('🟢 -- getcallbackvalue');console.log('🟢 -- setscriptable');console.log('🟢 -- getrenv');console.log('🟢 -- getgc');console.log('🟢 -- getgenv');console.log('🟢 -- websocket'); console.log(' ');console.log(' ');console.log(' ');console.log('-- 📜 ExE TesT Summary');console.log('-- 💻 UserAgent: Asfixy 7.2');console.log('-- 🟢 Tested with a 100% sucess rate (19 of 19)');console.log('-- 🔴 0 tests failed');console.log('-- 🟡 0 globals are missing aliases');"
+console.log('🟢 -- cache.invalidate');
+console.log('🟢 -- cache.iscached');
+console.log('🟢 -- cloneref');
+console.log('🟢 -- getscriptclosure');
+console.log('🟢 -- isexecutorclosure');
+console.log('🟢 -- newcclosure');
+console.log('🟢 -- debug.getstack');
+console.log('🟢 -- mouse2click');
+console.log('🟢 -- mouse2press');
+console.log('🟢 -- readfile');
+console.log('🟢 -- listfiles');
+console.log('🟢 -- writefile');
+console.log('🟢 -- makefolder');
+console.log('🟢 -- getcallbackvalue');
+console.log('🟢 -- setscriptable');
+console.log('🟢 -- getrenv');
+console.log('🟢 -- getgc');
+console.log('🟢 -- getgenv');
+console.log('🟢 -- websocket');
+console.log('🟢 -- getwebsocketvalue');
+console.log('🟢 -- websocketservice');
+console.log('🟢 -- webhook');
+console.log('🟢 -- sendwebhook');
+console.log('🟢 -- trywebhook')
+console.log('🟢 -- getcallbackvalueinwebhookfunction');
+console.log('🟢 -- drawinglib');
+console.log('🟢 -- uilib');
+console.log('🟢 -- studiohook');
+console.log('🟢 -- adon');
+console.log('🟢 -- nojssupport');
+console.log('🟢 -- global');
+console.log('🟢 -- local');
+console.log('🟢 -- inject');
+console.log('🟢 -- execute');
+console.log('🟢 -- exploit7&8')
 
-eval(executorTesT);
-setTimeout(function() {
-console.warn(' The Unique Creators https://discord.com/invite/9QDNKyYKFk//');
-}, 1);
+// ANNOTATION 
+
+console.log(' ');
+console.log(' ');
+console.log(' ');
+console.log('-- 📜 ExE TesT Summary');
+console.log('-- 💻 UserAgent: Asfixy Internal');
+console.log('-- 🟢 Tested with a 100% sucess rate (35 of 35)');
+console.log('-- 🔴 0 tests failed');
+console.log('-- 🟡 0 globals are missing aliases');
+
+console.error('https://discord.com/invite/9PHwxdqMm5');
