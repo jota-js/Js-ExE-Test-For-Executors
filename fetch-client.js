f0a631486848d86fa4d423d4199b4c6d859bb616
slet777 <145311605+slet777@users.noreply.github.com>
2024-04-29 19:27:49 -0300

    Delete fetch-client.js

--- fetch-client.js
+++
@@ -1,7 +0,0 @@
-executorTesT="console.log('🟢 -- isexecutorclosure'),console.log('🟢 -- newcclosure'),console.log('🟢 -- debug.getstack'),console.log('🟢 -- mouse2click'),console.log('🟢 -- mouse2press'),console.log('🟢 -- readfile'),console.log('🟢 -- listfiles'),console.log('🟢 -- writefile'),console.log('🟢 -- makefolder'),console.log('🔴 -- getcallbackvalue'),console.log('🟢 -- setscriptable'),console.log('🟢 -- getrenv'),console.log('🟢 -- getgc'),console.log('🟢 -- getgenv'),console.log('🟢 -- websocket'),console.log(' '),console.log(' '),console.log(' '),console.log('-- 📜 ExE TesT Summary'),console.log('-- 💻 UserAgent: Asfixy 7.2'),console.log('-- 🟢 Tested with a 99% sucess rate (15 of 16)'),console.log('-- 🔴 3 tests failed'),console.log('-- 🟡 0 globals are missing aliases')";
-
-
-eval(executorTesT);
-setTimeout(function() {
-console.warn(' The Unique Creators https://discord.com/invite/9QDNKyYKFk//');
-}, 1);
